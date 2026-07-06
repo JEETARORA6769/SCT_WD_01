a54cadaff6c753778dee3c6b1eb86e488489df89
JEETARORA6769 <ja3736@srmist.edu.in>
2026-07-06 23:11:25 +0530

    Day 1: Implement interactive navigation menu using JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("JavaScript Connected Successfully!");
+const navbar = document.getElementById("navbar");
+console.log(navbar);
+window.addEventListener("scroll", function () {
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    console.log(window.scrollY);
+});
