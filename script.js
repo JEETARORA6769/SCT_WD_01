c0559bd037b06b7a15c625d4673bb97b97e78fe7
JEETARORA6769 <ja3736@srmist.edu.in>
2026-07-07 15:22:13 +0530

    nhance navigation menu with mobile support and scroll interactions

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
-console.log("JavaScript Connected Successfully!");
+// Select Elements
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
 const navbar = document.getElementById("navbar");
-console.log(navbar);
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+// Navbar Scroll Effect
 window.addEventListener("scroll", function () {
 
-    if(window.scrollY > 50){
+    if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
-    } else{
+    } else {
         navbar.classList.remove("scrolled");
     }
-    console.log(window.scrollY);
+
+});
+
+// Active Navigation Highlight
+window.addEventListener("scroll", function () {
+
+    let currentSection = "";
+
+    sections.forEach(function(section){
+
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if(
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ){
+            currentSection = section.id;
+        }
+
+    });
+
+    navLinks.forEach(function(link){
+
+        link.classList.remove("active-link");
+
+        if(link.getAttribute("href") === "#" + currentSection){
+
+            link.classList.add("active-link");
+
+        }
+
+    });
+
+});
+
+// Hamburger Menu
+menuToggle.addEventListener("click", function () {
+
+    navMenu.classList.toggle("active");
+
+    if (navMenu.classList.contains("active")) {
+        menuToggle.textContent = "✖";
+    } else {
+        menuToggle.textContent = "☰";
+    }
+
+});
+
+// Close Menu After Clicking a Link
+navLinks.forEach(function(link){
+
+    link.addEventListener("click", function(){
+
+        navMenu.classList.remove("active");
+        menuToggle.textContent = "☰";
+
+    });
+
 });
